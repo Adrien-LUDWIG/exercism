31317734c1d2fe1fc714f11a130af3510dbb1b5e
Adrien-ANTON-LUDWIG <adrien.anton-ludwig@epita.fr>
2023-06-14 22:43:32 +0000

    racket/darts: add solution

---
+++ racket/darts/darts.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(provide score)
+
+; `score` returns the dart throw score given the dart's coordinates (x, y).
+(define (score x y)
+  (define (distance) (sqrt (+ (* x x) (* y y))))
+  (cond
+    [(<= (distance) 1) 10]
+    [(<= (distance) 5) 5]
+    [(<= (distance) 10) 1]
+    [else 0]))
