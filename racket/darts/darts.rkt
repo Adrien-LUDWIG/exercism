c0d018487953050b688168e0ebf6d96df943ef69
Adrien-ANTON-LUDWIG <adrien.anton-ludwig@epita.fr>
2023-06-14 22:51:15 +0000

    racket/darts: use `let*` just for fun

--- racket/darts/darts.rkt
+++ racket/darts/darts.rkt
@@ -4,9 +4,10 @@
 
 ; `score` returns the dart throw score given the dart's coordinates (x, y).
 (define (score x y)
-  (define (distance) (sqrt (+ (* x x) (* y y))))
-  (cond
-    [(<= (distance) 1) 10]
-    [(<= (distance) 5) 5]
-    [(<= (distance) 10) 1]
-    [else 0]))
+  (let* ([distance (sqrt (+ (* x x) (* y y)))]
+        [in (lambda (r) (<= distance r))])
+    (cond
+      [(in 1) 10]
+      [(in 5) 5]
+      [(in 10) 1]
+      [else 0])))
