9a843b4ae3dae154946815aab1d3a1de51f8dc7a
Adrien-ANTON-LUDWIG <adrien.anton-ludwig@epita.fr>
2023-06-14 19:05:28 +0000

    racket/grains: use `expt` instead of custom `pow`

--- racket/grains/grains.rkt
+++ racket/grains/grains.rkt
@@ -2,15 +2,9 @@
 
 (provide square total)
 
-; `pow` returns the power of `num` by `exp`.
-(define (pow num exp)
-  (cond
-    [(zero? exp) 1]
-    [else (* num (pow num (sub1 exp)))]))
-
 ; `square` returns the number of grains on the `a-square`th square.
 (define (square a-square)
-  (pow 2 (sub1 a-square)))
+  (expt 2 (sub1 a-square)))
 
 ; `sum-square-grains` returns the total number of grains on the chessboard.
 (define (sum-square-grains grains remaining-squares)
