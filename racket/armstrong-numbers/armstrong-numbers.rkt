d21047f496560e6f8095f20331f8b9eda6588680
Adrien-ANTON-LUDWIG <adrien.anton-ludwig@epita.fr>
2023-06-24 22:55:14 +0000

    racket/armstrong-numbers: make it tail recursive

--- racket/armstrong-numbers/armstrong-numbers.rkt
+++ racket/armstrong-numbers/armstrong-numbers.rkt
@@ -2,18 +2,19 @@
 
 (provide armstrong-number?)
 
-(define (get-nb-of-digits n)
+(define (get-nb-of-digits n [count 0])
   (cond
-    [(zero? n) 0]
-    [else (add1 (get-nb-of-digits (quotient n 10)))]))
+    [(zero? n) count]
+    [else (get-nb-of-digits (quotient n 10) (add1 count))]))
 
 ; `sum-of-powers` returns the sum of digits the digits of `n`,
 ; each to the power of `nb-of-digits`.
-(define (sum-of-powers n nb-of-digits)
+(define (sum-of-powers n nb-of-digits [total 0])
   (cond
-    [(zero? n) 0]
-    [else (+ (expt (modulo n 10) nb-of-digits)
-             (sum-of-powers (quotient n 10) nb-of-digits))]))
+    [(zero? n) total]
+    [else (sum-of-powers (quotient n 10)
+                         nb-of-digits
+                         (+ total (expt (modulo n 10) nb-of-digits)))]))
 
 (define (armstrong-number? n)
   (= n (sum-of-powers n (get-nb-of-digits n))))
