6d71a875ba370a5ffe4ad32975b90603ccc55610
Adrien-ANTON-LUDWIG <adrien.anton-ludwig@epita.fr>
2023-06-24 23:31:02 +0000

    racket/armstrong-numbers: add foldr-digits (DRY)

--- racket/armstrong-numbers/armstrong-numbers.rkt
+++ racket/armstrong-numbers/armstrong-numbers.rkt
@@ -2,19 +2,20 @@
 
 (provide armstrong-number?)
 
-(define (get-nb-of-digits n [count 0])
+; `foldr-digits` applies a procedure to the digits of a natural number,
+; from the right to the left.
+(define (foldr-digits proc init n)
   (cond
-    [(zero? n) count]
-    [else (get-nb-of-digits (quotient n 10) (add1 count))]))
+    [(zero? n) init]
+    [else (foldr-digits proc (proc (modulo n 10) init) (quotient n 10))]))
+
+(define (get-nb-of-digits n)
+  (foldr-digits (lambda (digit acc) (add1 acc)) 0 n))
 
 ; `sum-of-powers` returns the sum of digits the digits of `n`,
 ; each to the power of `nb-of-digits`.
-(define (sum-of-powers n nb-of-digits [total 0])
-  (cond
-    [(zero? n) total]
-    [else (sum-of-powers (quotient n 10)
-                         nb-of-digits
-                         (+ total (expt (modulo n 10) nb-of-digits)))]))
+(define (sum-of-powers n nb-of-digits)
+  (foldr-digits (lambda (digit acc) (+ acc (expt digit nb-of-digits))) 0 n))
 
 (define (armstrong-number? n)
   (= n (sum-of-powers n (get-nb-of-digits n))))
