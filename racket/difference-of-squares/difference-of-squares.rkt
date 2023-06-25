734cc27fe0bcef72418609bb47a9d08fbfd13391
Adrien-ANTON-LUDWIG <adrien.anton-ludwig@epita.fr>
2023-06-25 18:24:06 +0000

    racket/difference-of-squares: use sum formulas

--- racket/difference-of-squares/difference-of-squares.rkt
+++ racket/difference-of-squares/difference-of-squares.rkt
@@ -2,11 +2,13 @@
 
 (provide sum-of-squares square-of-sum difference)
 
+; Σn² = n(n+1)(2n+1) / 6
 (define (sum-of-squares number)
-  (for/sum ([x (in-inclusive-range 1 number)]) (expt x 2)))
+  (/ (* number (add1 number) (add1 (* 2 number))) 6))
 
+; (Σn)² = n(n+1) / 2 
 (define (square-of-sum number)
-  (expt (for/sum ([x (in-inclusive-range 1 number)]) x) 2))
+  (expt (/ (* number (add1 number)) 2) 2))
 
 (define (difference number)
   (- (square-of-sum number) (sum-of-squares number)))
