f9a6f3849a6d292cafaffc3779472ae96fa6f3f4
Adrien-ANTON-LUDWIG <adrien.anton-ludwig@epita.fr>
2023-06-25 23:45:33 +0000

    racket/perfect-numbers: use divsors

--- racket/perfect-numbers/perfect-numbers.rkt
+++ racket/perfect-numbers/perfect-numbers.rkt
@@ -4,29 +4,12 @@
 
 (require math/number-theory)
 
-(define (divisors number)
-  ; for each value x in the range
-  (stream-fold
-   ; check if x divides number
-   (lambda (acc x)
-     (cond
-       ; if it divides number
-       [(divides? x number)
-        (define q (quotient number x))
-        (cond
-          ; check if the quotient is x himself or number
-          [(or
-            (= q x)
-            (= q number))
-           (append acc (list x))]
-          ; if it is not, append it to the divisors list
-          [else (append acc (list x q))])]
-       [else acc]))
-   empty
-   (in-range 1 (sqrt number))))
+; aliquot sum does not include the number itself as a divisor
+(define (aliquot-divisors n)
+  (drop-right (divisors n) 1))
 
 (define (classify number)
-  (define aliquot-sum (apply + (divisors number)))
+  (define aliquot-sum (apply + (aliquot-divisors number)))
   (cond
     [(< aliquot-sum number) 'deficient]
     [(> aliquot-sum number) 'abundant]
