39c7a8f9d0523e9b0c7263ef64afd4a862c877b9
Adrien-LUDWIG <adrien.anton-ludwig@epita.fr>
2023-07-21 16:18:28 +0000

    racket/darts: use define
    As recommended by the Racket Style Guide, as pointed out to me in an
    mentoring session.

--- racket/darts/darts.rkt
+++ racket/darts/darts.rkt
@@ -1,12 +1,10 @@
 #lang racket
-
 (provide score)
-
 ; `score` returns the dart throw score given the dart's coordinates (x, y).
 (define (score x y)
-  (let ([distance (sqrt (+ (* x x) (* y y)))])
-    (cond
-      [(<= distance 1) 10]
-      [(<= distance 5) 5]
-      [(<= distance 10) 1]
-      [else 0])))
+  (define distance (sqrt (+ (* x x) (* y y))))
+  (cond
+    [(<= distance 1) 10]
+    [(<= distance 5) 5]
+    [(<= distance 10) 1]
+    [else 0]))
