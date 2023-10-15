5026ddefc2749c97ad8d2e708b88e5b5557cf26a
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-10-15 12:20:23 +0200

    cpp/elles-alien-game: add solution

---
+++ cpp/ellens-alien-game/ellens_alien_game.cpp
@@ -0,0 +1,35 @@
+namespace targets {
+    class Alien {
+       public:
+        Alien(int x, int y) {
+            x_coordinate = x;
+            y_coordinate = y;
+        }
+
+        int get_health() { return health; }
+
+        bool is_alive() { return health > 0; }
+
+        bool hit() {
+            if (is_alive()) health--;
+            return true;
+        }
+
+        bool teleport(int x_new, int y_new) {
+            x_coordinate = x_new;
+            y_coordinate = y_new;
+            return true;
+        }
+
+        bool collision_detection(Alien other) {
+            return x_coordinate == other.x_coordinate and
+                   y_coordinate == other.y_coordinate;
+        }
+
+        int x_coordinate{0};
+        int y_coordinate{0};
+
+       private:
+        int health{3};
+    };
+}  // namespace targets
