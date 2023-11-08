44f9afe217d9c297bfc99b5b34f68ada24694afe
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-11-08 23:33:53 +0100

    go/card-tricks: DRY and refactor RemoveItem

--- go/card-tricks/card_tricks.go
+++ go/card-tricks/card_tricks.go
@@ -1,5 +1,9 @@
 package cards
 
+func invalidIndex(slice []int, index int) bool {
+	return index < 0 || index >= len(slice)
+}
+
 // FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
 func FavoriteCards() []int {
 	return []int{2, 6, 9}
@@ -8,7 +12,7 @@ func FavoriteCards() []int {
 // GetItem retrieves an item from a slice at given position.
 // If the index is out of range, we want it to return -1.
 func GetItem(slice []int, index int) int {
-	if index < 0 || index >= len(slice) {
+	if invalidIndex(slice, index) {
 		return -1
 	}
 	return slice[index]
@@ -17,7 +21,7 @@ func GetItem(slice []int, index int) int {
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range the value needs to be appended.
 func SetItem(slice []int, index, value int) []int {
-	if index < 0 || index >= len(slice) {
+	if invalidIndex(slice, index) {
 		slice = append(slice, value)
 	} else {
 		slice[index] = value
@@ -33,13 +37,9 @@ func PrependItems(slice []int, values ...int) []int {
 
 // RemoveItem removes an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
-	if index < 0 || index >= len(slice) {
+	if invalidIndex(slice, index) {
 		return slice
 	}
 
-	for i := index + 1; i < len(slice); i++ {
-		slice[i-1] = slice[i]
-	}
-
-	return slice[:len(slice)-1]
+	return append(slice[:index], slice[index+1:]...)
 }
