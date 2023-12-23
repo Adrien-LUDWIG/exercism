453a16c1ac9592a3ee5f15ef7b07046db99cc82f
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-12-23 14:38:44 +0100

    bash/darts: use bc for conditions

--- bash/darts/darts.sh
+++ bash/darts/darts.sh
@@ -12,14 +12,17 @@ is_numeric() {
 (($# != 2)) && die "Usage: darts <x> <y>"
 (is_numeric "$1" && is_numeric "$2") || die "Arguments must be numeric values"
 
-distance=$(bc <<<"scale=3; dist=sqrt(($1*$1+$2*$2)); scale=0; dist * 1000 / 1")
+bc <<<"
+  scale=3
+  dist=sqrt(($1*$1+$2*$2))
 
-if [[ distance -gt 10000 ]]; then
-  echo "0"
-elif [[ distance -gt 5000 ]]; then
-  echo "1"
-elif [[ distance -gt 1000 ]]; then
-  echo "5"
-else # distance <= 10
-  echo "10"
-fi
+  if (dist > 10) {
+    0
+  } else if (dist > 5) {
+    1
+  } else if (dist > 1) {
+    5
+  } else {
+    10
+  }
+"
