c0d76b374a83e6de5b79bb2e877f45f139afd048
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-12-23 17:24:41 +0100

    bash/series: add solution

---
+++ bash/series/series.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+die() {
+  echo "$*"
+  exit 1
+}
+
+string=$1
+string_length=${#1}
+substring_length=$2
+
+((string_length == 0)) && die "series cannot be empty"
+((substring_length > string_length)) && die "slice length cannot be greater than series length"
+((substring_length == 0)) && die "slice length cannot be zero"
+((substring_length < 0)) && die "slice length cannot be negative"
+
+for i in $(seq 0 $((string_length - substring_length - 1))); do
+  echo -n "${string:i:substring_length} "
+done
+
+echo -n "${string:$((string_length - substring_length)):substring_length}"
