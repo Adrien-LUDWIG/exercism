e3b50a1377e47775ea42342b28494ea359f7a631
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-12-27 23:25:51 +0100

    elixir/hamming: compare heads with pattern match

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -12,17 +12,13 @@ defmodule Hamming do
     {:error, "strands must be of equal length"}
   end
 
-  def hamming_distance(string1, string2) when length(string1) == length(string2) do
+  def hamming_distance(string1, string2) do
     {:ok, hamming(string1, string2, 0)}
   end
 
   defp hamming(~c"", ~c"", diff_count), do: diff_count
+  defp hamming([head | tail1], [head | tail2], diff_count), do: hamming(tail1, tail2, diff_count)
 
-  defp hamming([head1 | tail1], [head2 | tail2], diff_count) when head1 == head2 do
-    hamming(tail1, tail2, diff_count)
-  end
-
-  defp hamming([head1 | tail1], [head2 | tail2], diff_count) when head1 != head2 do
-    hamming(tail1, tail2, diff_count + 1)
-  end
+  defp hamming([_head1 | tail1], [_head2 | tail2], diff_count),
+    do: hamming(tail1, tail2, diff_count + 1)
 end
