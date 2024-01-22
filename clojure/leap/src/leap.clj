72454669f491c9996c6ff62f86a78063bc9034b8
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2024-01-22 17:42:20 +0100

    clojure/leap: use `letfn` instead of inline `defn`

--- clojure/leap/src/leap.clj
+++ clojure/leap/src/leap.clj
@@ -1,7 +1,6 @@
 (ns leap)
 
 (defn leap-year? [year]
-  (defn divisor? [val]
-    (= (mod year val) 0))
-  (and (divisor? 4) (or (not (divisor? 100))
-                        (divisor? 400))))
+  (letfn [(divisor? [n] (zero? (mod year n)))]
+    (and (divisor? 4) (or (not (divisor? 100))
+                          (divisor? 400)))))
